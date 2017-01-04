c401a554a34f5485a204726082dea69a4e28c164
beanstown106 <nbenis106@gmail.com>
2017-01-04 13:29:17 -0500

    Pixel Navbar animation toggle [1/2]
    *made to work with the exposure for themers to disable navbar animation
    
    Change-Id: If15aff30447a65b9199d1768802ce86f1e1e1615
    Signed-off-by: spezi77 <spezi7713@gmx.net>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4956,6 +4956,12 @@ public final class Settings {
         public static final String AMBIENT_WAKE_GESTURES = "ambient_wake_gestures";
 
         /**
+         * Whether to enable the pixel navbar animation
+         * @hide
+         */
+        public static final String PIXEL_NAV_ANIMATION = "pixel_nav_animation";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
