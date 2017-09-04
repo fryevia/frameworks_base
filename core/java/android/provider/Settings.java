3223e0dbe7ade066c9fc8c0b3bb8b2a14869044b
Sam Mortimer <sam@mortimer.me.uk>
2017-09-04 18:56:52 +0200

    Battery light customization [1/2]
    abc ezio84: allow to disable battery light when Do Not Disturb is enabled
    use our margaritov color picker for the settings part
    removed the disable battery light toggle, never used it
    and we can just set the led to black if it really bothers
    
    -----
    
    https://review.lineageos.org/#/c/68217/
    
    Change-Id: Ia3d0a294311fdb73f271cc80a4b5327c44854a18
    
    Disable battery light overlay by default
    
    Change-Id: I8efe535702a45ed7445b8151dff61a05a909e5e7

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4858,6 +4858,42 @@ public final class Settings {
          */
         public static final String CUSTOM_RINGTONE_VIBRATION_PATTERN = "custom_ringtone_vibration_pattern";
 
+       /**
+        * Whether to show battery light when DND mode is active
+        * @hide
+        */
+       public static final String BATTERY_LIGHT_ALLOW_ON_DND = "battery_light_allow_on_dnd";
+
+       /**
+        * Whether to show blinking light when battery is low
+        * @hide
+        */
+       public static final String BATTERY_LIGHT_LOW_BLINKING = "battery_light_low_blinking";
+
+       /**
+        * Low battery charging color
+        * @hide
+        */
+       public static final String BATTERY_LIGHT_LOW_COLOR = "battery_light_low_color";
+
+       /**
+        * Medium battery charging color
+        * @hide
+        */
+       public static final String BATTERY_LIGHT_MEDIUM_COLOR = "battery_light_medium_color";
+
+       /**
+        * Full battery charging color
+        * @hide
+        */
+       public static final String BATTERY_LIGHT_FULL_COLOR = "battery_light_full_color";
+
+       /**
+        * Really full 100 battery charging color
+        * @hide
+        */
+       public static final String BATTERY_LIGHT_REALLYFULL_COLOR = "battery_light_reallyfull_color";
+
         /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
