bad1a6069799345a930430bea369bcf0f28db596
Pranav Vashi <neobuddy89@gmail.com>
2020-01-16 00:37:03 +0530

    SystemUI: Cancel ticker if notification entry removed
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Change-Id: Ifa5b1d18fe8acaf76b1ef4de7fd32e0d5d61ffd3

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
@@ -3322,6 +3322,14 @@ public class StatusBar extends SystemUI implements DemoMode,
         }
     }
 
+    public Ticker getTicker() {
+        return mTicker;
+    }
+
+    public boolean isTickerEnabled() {
+        return mTicker != null && mTickerEnabled != 0;
+    }
+
     public void haltTicker() {
         if (mTicker != null && mTickerEnabled != 0) {
             mTicker.halt();
