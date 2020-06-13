f2930ee4e9415a61364c05e41593b71973298784
SuperDroidBond <superdroidbond@yahoo.com>
2020-06-13 23:28:22 +0530

    KG Clock: add new variant Oronos Square
    Change-Id: Ia2b8c7d75a88f20f22b438456df8c4678e5ee921

--- packages/SystemUI/src/com/android/keyguard/clock/ClockManager.java
+++ packages/SystemUI/src/com/android/keyguard/clock/ClockManager.java
@@ -165,6 +165,7 @@ public final class ClockManager {
         addBuiltinClock(() -> new MNMLBoxClockController(res, layoutInflater, colorExtractor));
         addBuiltinClock(() -> new MNMLMinimalClockController(res, layoutInflater, colorExtractor));
         addBuiltinClock(() -> new OronosClockController(res, layoutInflater, colorExtractor));
+        addBuiltinClock(() -> new OronosSqrClockController(res, layoutInflater, colorExtractor));
 
         // Store the size of the display for generation of clock preview.
         DisplayMetrics dm = res.getDisplayMetrics();
