14b9c6812253dc6cf491c40b36afca52593fc7dc
SagarMakhar <sagarmakhar@gmail.com>
2020-10-08 08:39:54 +0000

    SystemUI: Decrease QS rows by 1 when qs media is showing
    - 3 rows if media isn't showing , 2 otherwise.
    - Always force 2 rows in landscape
    
    Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>
    Change-Id: I609ab8027f7085004bbea96c4deac23b74e71969

--- packages/SystemUI/src/com/android/systemui/qs/QSPanel.java
+++ packages/SystemUI/src/com/android/systemui/qs/QSPanel.java
@@ -136,6 +136,8 @@ public class QSPanel extends LinearLayout implements Tunable, Callback, Brightne
     private int mVisualTilePadding;
     private boolean mUsingHorizontalLayout;
 
+    private boolean mQsMediaVisible;
+
     private QSCustomizer mCustomizePanel;
     private Record mDetailRecord;
 
@@ -223,6 +225,7 @@ public class QSPanel extends LinearLayout implements Tunable, Callback, Brightne
     }
 
     protected void onMediaVisibilityChanged(Boolean visible) {
+        mQsMediaVisible = visible;
         switchTileLayout();
         if (mMediaVisibilityChangedListener != null) {
             mMediaVisibilityChangedListener.accept(visible);
@@ -548,7 +551,7 @@ public class QSPanel extends LinearLayout implements Tunable, Callback, Brightne
                 == Configuration.ORIENTATION_LANDSCAPE ? true : false;
         if (newConfig.orientation != mLastOrientation) {
             mLastOrientation = newConfig.orientation;
-            switchTileLayout();
+            switchTileLayout(true);
         }
 
         if (mOPFooterView != null) mOPFooterView.setOrientation(mIsLandscape);
@@ -576,12 +579,19 @@ public class QSPanel extends LinearLayout implements Tunable, Callback, Brightne
     private boolean switchTileLayout(boolean force) {
         /** Whether or not the QuickQSPanel currently contains a media player. */
         boolean horizontal = shouldUseHorizontalLayout();
+        QSTileLayout newLayout = horizontal ? mHorizontalTileLayout : mRegularTileLayout;
+        if (needsDynamicRowsAndColumns()) {
+            int rows = (mQsMediaVisible || getResources().getConfiguration().orientation
+                == Configuration.ORIENTATION_LANDSCAPE) ? 2 : TileLayout.NO_MAX_ROWS;
+            newLayout.setMinRows(horizontal ? 2 : rows);
+            // Let's use 3 columns to match the current layout
+            newLayout.setMaxColumns(horizontal ? 3 : TileLayout.NO_MAX_COLUMNS);
+        }
         if (horizontal != mUsingHorizontalLayout || force) {
             mUsingHorizontalLayout = horizontal;
             View visibleView = horizontal ? mHorizontalLinearLayout : (View) mRegularTileLayout;
             View hiddenView = horizontal ? (View) mRegularTileLayout : mHorizontalLinearLayout;
             ViewGroup newParent = horizontal ? mHorizontalContentContainer : this;
-            QSTileLayout newLayout = horizontal ? mHorizontalTileLayout : mRegularTileLayout;
             if (hiddenView != null &&
                     (mRegularTileLayout != mHorizontalTileLayout ||
                             hiddenView != mRegularTileLayout)) {
@@ -602,11 +612,6 @@ public class QSPanel extends LinearLayout implements Tunable, Callback, Brightne
             mTileLayout = newLayout;
             if (mHost != null) setTiles(mHost.getTiles());
             newLayout.setListening(mListening);
-            if (needsDynamicRowsAndColumns()) {
-                newLayout.setMinRows(horizontal ? 2 : 1);
-                // Let's use 3 columns to match the current layout
-                newLayout.setMaxColumns(horizontal ? 3 : TileLayout.NO_MAX_COLUMNS);
-            }
             updateTileLayoutMargins();
             updateFooterMargin();
             updateMediaDisappearParameters();
