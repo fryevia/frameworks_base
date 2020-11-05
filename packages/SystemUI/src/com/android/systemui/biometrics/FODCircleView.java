a1d17099a72ab041f31b2753badd7067da3c9071
TheScarastic <warabhishek@gmail.com>
2020-11-05 22:48:58 +0530

    [SQUASH] FODCircleView: Hide FOD while going to sleep and make it configurable
     Author:    TheScarastic <warabhishek@gmail.com>
     Date:      Thu Nov 5 22:48:58 2020 +0530
    
        FODCircleView: Hide fod while going to sleep
    Change-Id: I2cab6231c8408256eb2a24e7fbf9dd36e1824500
    
    commit 35c1375e00b24680d2484e4e715bfd95bafd636c
    Author: kubersharma001 <kubersharma001@gmail.com>
    Date:   Tue Nov 10 18:57:00 2020 +0100
    
        Make "Hide fod while going to sleep" configurable
    
        `Hide fod while going to sleep` fixes High brightness flicker while going to sleep from lockscreen on
    raphael BUT it causes the FOD icon to glitch out while on AOD on
    davinci. Hence make this configurable via an overlay to restore proper
    behaviour for both device's FOD impl.
    
        test:
        On raphael (prebuilt OEM vendor|kernel):
        set the overlay to true on raphael, set FOD, lock screen and press
    power button and notice that brightness doesn't jump to 100% while
    turning the screen off
    
        On davinci (OSS vendor|kernel):
        by default the overlay is set to false, set FOD on davinci, enable
    AOD and try to lock the phone with aod, unlock without fingerprint
    (power button), lock again and FOD icon will not glitch out like before
    
        Change-Id: I84ce84b8d235de9b8407683900a23f9b404ee8a9
    
    Co-authored-by: Kuber Sharma <kubersharma001@gmail.com>
    Change-Id: Ib2f5b0a1dbd73242a08bf39f7f84aa280574a34a
    Signed-off-by: Joey Huab <joey@evolution-x.org>

--- packages/SystemUI/src/com/android/systemui/biometrics/FODCircleView.java
+++ packages/SystemUI/src/com/android/systemui/biometrics/FODCircleView.java
@@ -74,6 +74,7 @@ public class FODCircleView extends ImageView implements TunerService.Tunable, Co
     private final int mSize;
     private final int mDreamingMaxOffset;
     private final int mNavigationBarSize;
+    private final boolean mHideFodCircleGoingToSleep;
     private final boolean mShouldBoostBrightness;
     private final Paint mPaintFingerprintBackground = new Paint();
     private final LayoutParams mParams = new LayoutParams();
@@ -223,9 +224,17 @@ public class FODCircleView extends ImageView implements TunerService.Tunable, Co
         }
 
         @Override
+        public void onStartedGoingToSleep(int why) {
+            if (mHideFodCircleGoingToSleep) {
+                hide();
+            }
+        }
+
+        @Override
         public void onScreenTurnedOff() {
             mScreenTurnedOn = false;
-            if (!mFodGestureEnable) {
+            if (!mFodGestureEnable &&
+                    !mHideFodCircleGoingToSleep) {
                 hide();
             } else {
                 hideCircle();
@@ -291,6 +300,9 @@ public class FODCircleView extends ImageView implements TunerService.Tunable, Co
         mPowerManager = context.getSystemService(PowerManager.class);
         mWakeLock = mPowerManager.newWakeLock(PowerManager.PARTIAL_WAKE_LOCK,
                  FODCircleView.class.getSimpleName());
+
+        mHideFodCircleGoingToSleep = mContext.getResources().getBoolean(
+                com.android.internal.R.bool.config_hideFodCircleGoingToSleep);
 
         mWindowManager = mContext.getSystemService(WindowManager.class);
         mIsFodAnimationAvailable = derpUtils.isPackageInstalled(context,
