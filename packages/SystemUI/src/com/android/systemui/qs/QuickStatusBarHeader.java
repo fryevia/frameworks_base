0f8a3473801a513924e8c5b607e6747bcc585298
Dil3mm4 <dil3mm4.dev@gmail.com>
2020-12-29 14:56:35 +0900

    QuickStatusBarHeader: spawn blur if qs has been force expanded
    Test:
    On lockscreen, drag statusbar down to show QS, observe blur spawn
    
    Change-Id: I02ee4c7a9e628ae310ea35f499e394d6663e4d11

--- packages/SystemUI/src/com/android/systemui/qs/QuickStatusBarHeader.java
+++ packages/SystemUI/src/com/android/systemui/qs/QuickStatusBarHeader.java
@@ -72,6 +72,7 @@ import com.android.systemui.Dependency;
 import com.android.systemui.DualToneHandler;
 import com.android.systemui.Interpolators;
 import com.android.systemui.R;
+import com.android.systemui.dump.DumpManager;
 import com.android.systemui.plugins.ActivityStarter;
 import com.android.systemui.plugins.DarkIconDispatcher;
 import com.android.systemui.plugins.DarkIconDispatcher.DarkReceiver;
@@ -82,6 +83,7 @@ import com.android.systemui.privacy.PrivacyItem;
 import com.android.systemui.privacy.PrivacyItemController;
 import com.android.systemui.qs.QSDetail.Callback;
 import com.android.systemui.qs.carrier.QSCarrierGroup;
+import com.android.systemui.statusbar.BlurUtils;
 import com.android.systemui.statusbar.CommandQueue;
 import com.android.systemui.statusbar.phone.StatusBarIconController;
 import com.android.systemui.statusbar.phone.StatusBarIconController.TintedIconManager;
@@ -127,6 +129,7 @@ public class QuickStatusBarHeader extends RelativeLayout implements
     private final StatusBarIconController mStatusBarIconController;
     private final ActivityStarter mActivityStarter;
     private final Vibrator mVibrator;
+    private final BlurUtils mBlurUtils;
 
     private QSPanel mQsPanel;
 
@@ -237,6 +240,7 @@ public class QuickStatusBarHeader extends RelativeLayout implements
         mRingerModeTracker = ringerModeTracker;
         mUiEventLogger = uiEventLogger;
         mVibrator = (Vibrator) context.getSystemService(Context.VIBRATOR_SERVICE);
+        mBlurUtils = new BlurUtils(mContext.getResources(), new DumpManager());
     }
 
     @Override
@@ -527,6 +531,10 @@ public class QuickStatusBarHeader extends RelativeLayout implements
         }
 
         if (forceExpanded) {
+            if (mBlurUtils.supportsBlursOnWindows()) {
+                mBlurUtils.applyBlur(getViewRootImpl(),
+                        mBlurUtils.blurRadiusOfRatio(expansionFraction));
+            }
             // If the keyguard is showing, we want to offset the text so that it comes in at the
             // same time as the panel as it slides down.
             mHeaderTextContainerView.setTranslationY(panelTranslationY);
