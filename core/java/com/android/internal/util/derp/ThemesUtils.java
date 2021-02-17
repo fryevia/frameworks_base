216d968418837a5628f400c402721e6e23314901
Maitreya29 <maitreyapatni30@gmail.com>
2021-02-17 02:51:11 +0100

    base: Implement Gvisual mod navbar colors [1/3]
    Change-Id: I2158c22e727e293e3f02b9ddf614a943848ad587
    Signed-off-by: NurKeinNeid <mralexman3000@gmail.com>

--- core/java/com/android/internal/util/derp/ThemesUtils.java
+++ core/java/com/android/internal/util/derp/ThemesUtils.java
@@ -54,4 +54,10 @@ public class ThemesUtils {
             "com.gnonymous.gvisualmod.urm_m", // 2
             "com.gnonymous.gvisualmod.urm_l", // 3
     };
+
+    public static final String NAVBAR_COLOR_PURP = "com.gnonymous.gvisualmod.pgm_purp";
+
+    public static final String NAVBAR_COLOR_ORCD = "com.gnonymous.gvisualmod.pgm_orcd";
+
+    public static final String NAVBAR_COLOR_OPRD = "com.gnonymous.gvisualmod.pgm_oprd";
 }
