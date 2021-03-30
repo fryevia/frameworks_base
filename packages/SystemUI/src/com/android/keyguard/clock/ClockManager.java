d40759457abbc6c74c379964b23b274dcee40f64
mezz <mezzlasha@gmail.com>
2021-03-30 16:08:23 +0000

    SystemUI: Add OxygenOS lockscreen clock
    DerpFest edit: So let's rename it to OOS then
    SystemUI: Fix ShapeShift clock on 24h format
    SystemUI: Fix up preview for ShapeShift clock
    
    imjyotiraditya: SystemUI: OOSClock: Format and Cleanup
    
    Change-Id: I865c98d5a04745132993e6d3c6181888b200de4b
    Signed-off-by: Jyotiraditya Panda <jyotiraditya@aospa.co>
    Signed-off-by: Adithya R <gh0strider.2k18.reborn@gmail.com>

--- packages/SystemUI/src/com/android/keyguard/clock/ClockManager.java
+++ packages/SystemUI/src/com/android/keyguard/clock/ClockManager.java
@@ -171,7 +171,7 @@ public final class ClockManager {
         addBuiltinClock(() -> new GradientFireClockController(res, layoutInflater, colorExtractor));
         addBuiltinClock(() -> new TypeAltClockController(res, layoutInflater, colorExtractor));
         addBuiltinClock(() -> new StickerTagClockController(res, layoutInflater, colorExtractor));
-
+        addBuiltinClock(() -> new OOSClockController(res, layoutInflater, colorExtractor));
         // Store the size of the display for generation of clock preview.
         DisplayMetrics dm = res.getDisplayMetrics();
         mWidth = dm.widthPixels;
