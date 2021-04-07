c846556fdc89449966a8f5c493bb71748c8aafd0
El Dainosor <eldainosor@gmail.com>
2021-04-07 23:30:24 +0000

    Oroño square: it's not hip to be square
    Lets make it more adaptive, if you will call it that way

--- packages/SystemUI/src/com/android/keyguard/clock/ClockManager.java
+++ packages/SystemUI/src/com/android/keyguard/clock/ClockManager.java
@@ -165,7 +165,7 @@ public final class ClockManager {
         addBuiltinClock(() -> new MNMLBoxClockController(res, layoutInflater, colorExtractor));
         addBuiltinClock(() -> new MNMLMinimalClockController(res, layoutInflater, colorExtractor));
         addBuiltinClock(() -> new OronosClockController(res, layoutInflater, colorExtractor));
-        addBuiltinClock(() -> new OronosSqrClockController(res, layoutInflater, colorExtractor));
+        addBuiltinClock(() -> new OronosReshapedClockController(res, layoutInflater, colorExtractor));
 
         // Store the size of the display for generation of clock preview.
         DisplayMetrics dm = res.getDisplayMetrics();
