Octavi-OS/platform_frameworks_base

SystemUI: Accent color for Lockscreen ClockWidgets [1/2]
Signed-off-by: Sagarrokade006 <sagarrokade006@gmail.com>

packages/SystemUI/src/com/android/keyguard/clock/AnalogClockController.java
@@ -24,11 +24,14 @@ import android.graphics.Paint.Style;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.TextClock;
+import android.provider.Settings;
+import android.content.Context;
 
 import com.android.internal.colorextraction.ColorExtractor;
 import com.android.systemui.R;
 import com.android.systemui.colorextraction.SysuiColorExtractor;
 import com.android.systemui.plugins.ClockPlugin;
+import com.android.internal.util.octavi.OctaviUtils;
 
 import java.util.TimeZone;
 
@@ -74,6 +77,8 @@ public class AnalogClockController implements ClockPlugin {
     private View mView;
     private TextClock mLockClock;
 
+    private Context mContext;
+
     /**
      * Helper to extract colors from wallpaper palette for clock face.
      */
@@ -87,10 +92,11 @@ public class AnalogClockController implements ClockPlugin {
      * @param colorExtractor Extracts accent color from wallpaper.
      */
     public AnalogClockController(Resources res, LayoutInflater inflater,
-            SysuiColorExtractor colorExtractor) {
+            SysuiColorExtractor colorExtractor, Context context) {
         mResources = res;
         mLayoutInflater = inflater;
         mColorExtractor = colorExtractor;
+        mContext = context;
         mClockPosition = new SmallClockPosition(res);
     }
 
@@ -179,9 +185,15 @@ public class AnalogClockController implements ClockPlugin {
 
     private void updateColor() {
         final int primary = mPalette.getPrimaryColor();
-        final int secondary = mPalette.getSecondaryColor();
-        mLockClock.setTextColor(secondary);
-        mAnalogClock.setClockColors(primary, secondary);
+
+        if(OctaviUtils.useLockscreenClockAccentColor(mContext)) {
+            mLockClock.setTextColor(mContext.getResources().getColor(R.color.lockscreen_clock_accent_color));
+            mAnalogClock.setClockColors(primary, mContext.getResources().getColor(R.color.lockscreen_clock_accent_color));
+        } else {
+            final int secondary = mPalette.getSecondaryColor();
+            mLockClock.setTextColor(secondary);
+            mAnalogClock.setClockColors(primary, secondary);
+        }
     }
 
     @Override

packages/SystemUI/src/com/android/keyguard/clock/BubbleClockController.java
@@ -24,11 +24,14 @@ import android.graphics.Paint.Style;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.TextClock;
+import android.provider.Settings;
+import android.content.Context;
 
 import com.android.internal.colorextraction.ColorExtractor;
 import com.android.systemui.R;
 import com.android.systemui.colorextraction.SysuiColorExtractor;
 import com.android.systemui.plugins.ClockPlugin;
+import com.android.internal.util.octavi.OctaviUtils;
 
 import java.util.TimeZone;
 
@@ -74,6 +77,8 @@ public class BubbleClockController implements ClockPlugin {
     private View mLockClockContainer;
     private TextClock mLockClock;
 
+    private Context mContext;
+
     /**
      * Helper to extract colors from wallpaper palette for clock face.
      */
@@ -87,10 +92,11 @@ public class BubbleClockController implements ClockPlugin {
      * @param colorExtractor Extracts accent color from wallpaper.
      */
     public BubbleClockController(Resources res, LayoutInflater inflater,
-            SysuiColorExtractor colorExtractor) {
+            SysuiColorExtractor colorExtractor, Context context) {
         mResources = res;
         mLayoutInflater = inflater;
         mColorExtractor = colorExtractor;
+        mContext = context;
         mClockPosition = new SmallClockPosition(res);
     }
 
@@ -179,9 +185,15 @@ public class BubbleClockController implements ClockPlugin {
 
     private void updateColor() {
         final int primary = mPalette.getPrimaryColor();
-        final int secondary = mPalette.getSecondaryColor();
-        mLockClock.setTextColor(secondary);
-        mAnalogClock.setClockColors(primary, secondary);
+
+        if(OctaviUtils.useLockscreenClockAccentColor(mContext)) {
+            mLockClock.setTextColor(mContext.getResources().getColor(R.color.lockscreen_clock_accent_color));
+            mAnalogClock.setClockColors(primary, mContext.getResources().getColor(R.color.lockscreen_clock_accent_color));
+        } else {
+            final int secondary = mPalette.getSecondaryColor();
+            mLockClock.setTextColor(secondary);
+            mAnalogClock.setClockColors(primary, secondary);
+        }
     }
 
     @Override

packages/SystemUI/src/com/android/keyguard/clock/DefaultBoldClockController.java
@@ -25,11 +25,14 @@ import android.text.Html;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.TextClock;
+import android.provider.Settings;
+import android.content.Context;
 
 import com.android.internal.colorextraction.ColorExtractor;
 import com.android.systemui.R;
 import com.android.systemui.colorextraction.SysuiColorExtractor;
 import com.android.systemui.plugins.ClockPlugin;
+import com.android.internal.util.octavi.OctaviUtils;
 
 import java.util.TimeZone;
 
@@ -71,6 +74,8 @@ public class DefaultBoldClockController implements ClockPlugin {
      */
     private TextClock mClock;
 
+    private Context mContext;
+
     /**
      * Create a DefaultClockController instance.
      *
@@ -79,10 +84,11 @@ public class DefaultBoldClockController implements ClockPlugin {
      * @param colorExtractor Extracts accent color from wallpaper.
      */
     public DefaultBoldClockController(Resources res, LayoutInflater inflater,
-            SysuiColorExtractor colorExtractor) {
+            SysuiColorExtractor colorExtractor, Context context) {
         mResources = res;
         mLayoutInflater = inflater;
         mColorExtractor = colorExtractor;
+        mContext = context;
     }
 
     private void createViews() {
@@ -157,7 +163,11 @@ public class DefaultBoldClockController implements ClockPlugin {
 
     @Override
     public void setTextColor(int color) {
-        mClock.setTextColor(color);
+        if(OctaviUtils.useLockscreenClockAccentColor(mContext)) {
+            mClock.setTextColor(mContext.getResources().getColor(R.color.lockscreen_clock_accent_color));
+        } else {
+            mClock.setTextColor(color);
+        }
     }
 
     @Override

packages/SystemUI/src/com/android/keyguard/clock/DefaultClockController.java
@@ -24,11 +24,14 @@ import android.graphics.Paint.Style;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.TextView;
+import android.provider.Settings;
+import android.content.Context;
 
 import com.android.internal.colorextraction.ColorExtractor;
 import com.android.systemui.R;
 import com.android.systemui.colorextraction.SysuiColorExtractor;
 import com.android.systemui.plugins.ClockPlugin;
+import com.android.internal.util.octavi.OctaviUtils;
 
 import java.util.TimeZone;
 
@@ -72,6 +75,8 @@ public class DefaultClockController implements ClockPlugin {
      */
     private TextView mTextDate;
 
+     private Context mContext;
+
     /**
      * Create a DefaultClockController instance.
      *
@@ -80,10 +85,11 @@ public class DefaultClockController implements ClockPlugin {
      * @param colorExtractor Extracts accent color from wallpaper.
      */
     public DefaultClockController(Resources res, LayoutInflater inflater,
-            SysuiColorExtractor colorExtractor) {
+            SysuiColorExtractor colorExtractor, Context context) {
         mResources = res;
         mLayoutInflater = inflater;
         mColorExtractor = colorExtractor;
+        mContext = context;
     }
 
     private void createViews() {
@@ -154,8 +160,12 @@ public class DefaultClockController implements ClockPlugin {
 
     @Override
     public void setTextColor(int color) {
-        mTextTime.setTextColor(color);
-        mTextDate.setTextColor(color);
+        if(OctaviUtils.useLockscreenClockAccentColor(mContext)) {
+            mTextTime.setTextColor((mContext.getResources().getColor(R.color.lockscreen_clock_accent_color)));
+        } else {
+            mTextTime.setTextColor(color);
+            mTextDate.setTextColor(color);
+        }
     }
 
     @Override

packages/SystemUI/src/com/android/keyguard/clock/SfunyClockController.java
@@ -25,11 +25,14 @@ import android.text.Html;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.TextClock;
+import android.provider.Settings;
+import android.content.Context;
 
 import com.android.internal.colorextraction.ColorExtractor;
 import com.android.systemui.R;
 import com.android.systemui.colorextraction.SysuiColorExtractor;
 import com.android.systemui.plugins.ClockPlugin;
+import com.android.internal.util.octavi.OctaviUtils;
 
 import java.util.TimeZone;
 
@@ -72,6 +75,8 @@ public class SfunyClockController implements ClockPlugin {
     private TextClock mHourClock;
     private TextClock mMinuteClock;
 
+    private Context mContext;
+
     /**
      * Create a DefaultClockController instance.
      *
@@ -80,10 +85,11 @@ public class SfunyClockController implements ClockPlugin {
      * @param colorExtractor Extracts accent color from wallpaper.
      */
     public SfunyClockController(Resources res, LayoutInflater inflater,
-            SysuiColorExtractor colorExtractor) {
+            SysuiColorExtractor colorExtractor, Context context) {
         mResources = res;
         mLayoutInflater = inflater;
         mColorExtractor = colorExtractor;
+        mContext = context;
     }
 
     private void createViews() {
@@ -158,8 +164,13 @@ public class SfunyClockController implements ClockPlugin {
 
     @Override
     public void setTextColor(int color) {
-        mHourClock.setTextColor(color);
-        mMinuteClock.setTextColor(color);
+        if(Utils.useLockscreenClockAccentColor(mContext)) {
+            mLockClock.setTextColor(mContext.getResources().getColor(R.color.lockscreen_clock_accent_color));
+            mAnalogClock.setClockColors(primary, mContext.getResources().getColor(R.color.lockscreen_clock_accent_color));
+        } else {
+            mHourClock.setTextColor(color);
+            mMinuteClock.setTextColor(color);
+        }
     }
 
     @Override

packages/SystemUI/src/com/android/keyguard/clock/TypeClockController.java
@@ -24,11 +24,14 @@ import android.graphics.Paint.Style;
 import android.util.MathUtils;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.provider.Settings;
+import android.content.Context;
 
 import com.android.internal.colorextraction.ColorExtractor;
 import com.android.systemui.R;
 import com.android.systemui.colorextraction.SysuiColorExtractor;
 import com.android.systemui.plugins.ClockPlugin;
+import com.android.internal.util.octavi.OctaviUtils;
 
 import java.util.TimeZone;
 
@@ -72,6 +75,8 @@ public class TypeClockController implements ClockPlugin {
     private View mView;
     private TypographicClock mTypeClock;
 
+    private Context mContext;
+
     /**
      * Small clock shown on lock screen above stack scroller.
      */
@@ -90,7 +95,7 @@ public class TypeClockController implements ClockPlugin {
      * @param colorExtractor Extracts accent color from wallpaper.
      */
     TypeClockController(Resources res, LayoutInflater inflater,
-            SysuiColorExtractor colorExtractor) {
+            SysuiColorExtractor colorExtractor, Context context) {
         mResources = res;
         mLayoutInflater = inflater;
         mColorExtractor = colorExtractor;
@@ -184,8 +189,13 @@ public class TypeClockController implements ClockPlugin {
 
     @Override
     public void setTextColor(int color) {
-        mTypeClock.setTextColor(color);
-        mLockClock.setTextColor(color);
+        if(OctaviUtils.useLockscreenClockAccentColor(mContext)) {
+            mTypeClock.setTextColor(mContext.getResources().getColor(R.color.lockscreen_clock_accent_color));
+            mLockClock.setTextColor(mContext.getResources().getColor(R.color.lockscreen_clock_accent_color));
+        } else {
+            mTypeClock.setTextColor(color);
+            mLockClock.setTextColor(color);
+        }
     }
 
     @Override
@@ -194,8 +204,12 @@ public class TypeClockController implements ClockPlugin {
             return;
         }
         final int color = colorPalette[Math.max(0, colorPalette.length - 5)];
-        mTypeClock.setClockColor(color);
-        mLockClock.setClockColor(color);
+        if(OctaviUtils.useLockscreenClockAccentColor(mContext)) {
+            mTypeClock.setClockColor(mContext.getResources().getColor(R.color.lockscreen_clock_accent_color));
+        } else {
+            mTypeClock.setClockColor(color);
+            mLockClock.setClockColor(color);
+        }
     }
 
     @Override

fryevia/frameworks_base

Signed-off-by: NurKeinNeid <mralexman3000@gmail.com>

packages/SystemUI/src/com/android/keyguard/clock/AnalogClockController.java
@@ -25,11 +25,14 @@ import android.graphics.Typeface;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.TextClock;
+import android.provider.Settings;
+import android.content.Context;
 
 import com.android.internal.colorextraction.ColorExtractor;
 import com.android.systemui.R;
 import com.android.systemui.colorextraction.SysuiColorExtractor;
 import com.android.systemui.plugins.ClockPlugin;
+import com.android.internal.util.derp.derpUtils;
 
 import java.util.TimeZone;
 
@@ -75,6 +78,8 @@ public class AnalogClockController implements ClockPlugin {
     private View mView;
     private TextClock mLockClock;
 
+    private Context mContext;
+
     /**
      * Helper to extract colors from wallpaper palette for clock face.
      */
@@ -88,10 +93,11 @@ public class AnalogClockController implements ClockPlugin {
      * @param colorExtractor Extracts accent color from wallpaper.
      */
     public AnalogClockController(Resources res, LayoutInflater inflater,
-            SysuiColorExtractor colorExtractor) {
+            SysuiColorExtractor colorExtractor, Context context) {
         mResources = res;
         mLayoutInflater = inflater;
         mColorExtractor = colorExtractor;
+        mContext = context;
         mClockPosition = new SmallClockPosition(res);
     }
 
@@ -188,9 +194,15 @@ public class AnalogClockController implements ClockPlugin {
 
     private void updateColor() {
         final int primary = mPalette.getPrimaryColor();
-        final int secondary = mPalette.getSecondaryColor();
-        mLockClock.setTextColor(secondary);
-        mAnalogClock.setClockColors(primary, secondary);
+
+        if(derpUtils.useLockscreenClockAccentColor(mContext)) {
+            mLockClock.setTextColor(mContext.getResources().getColor(R.color.lockscreen_clock_accent_color));
+            mAnalogClock.setClockColors(primary, mContext.getResources().getColor(R.color.lockscreen_clock_accent_color));
+        } else {
+            final int secondary = mPalette.getSecondaryColor();
+            mLockClock.setTextColor(secondary);
+            mAnalogClock.setClockColors(primary, secondary);
+        }
     }
 
     @Override

packages/SystemUI/src/com/android/keyguard/clock/BubbleClockController.java
@@ -25,11 +25,14 @@ import android.graphics.Typeface;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.TextClock;
+import android.provider.Settings;
+import android.content.Context;
 
 import com.android.internal.colorextraction.ColorExtractor;
 import com.android.systemui.R;
 import com.android.systemui.colorextraction.SysuiColorExtractor;
 import com.android.systemui.plugins.ClockPlugin;
+import com.android.internal.util.derp.derpUtils;
 
 import java.util.TimeZone;
 
@@ -75,6 +78,8 @@ public class BubbleClockController implements ClockPlugin {
     private View mLockClockContainer;
     private TextClock mLockClock;
 
+    private Context mContext;
+
     /**
      * Helper to extract colors from wallpaper palette for clock face.
      */
@@ -88,10 +93,11 @@ public class BubbleClockController implements ClockPlugin {
      * @param colorExtractor Extracts accent color from wallpaper.
      */
     public BubbleClockController(Resources res, LayoutInflater inflater,
-            SysuiColorExtractor colorExtractor) {
+            SysuiColorExtractor colorExtractor, Context context) {
         mResources = res;
         mLayoutInflater = inflater;
         mColorExtractor = colorExtractor;
+        mContext = context;
         mClockPosition = new SmallClockPosition(res);
     }
 
@@ -188,9 +194,15 @@ public class BubbleClockController implements ClockPlugin {
 
     private void updateColor() {
         final int primary = mPalette.getPrimaryColor();
-        final int secondary = mPalette.getSecondaryColor();
-        mLockClock.setTextColor(secondary);
-        mAnalogClock.setClockColors(primary, secondary);
+
+        if(derpUtils.useLockscreenClockAccentColor(mContext)) {
+            mLockClock.setTextColor(mContext.getResources().getColor(R.color.lockscreen_clock_accent_color));
+            mAnalogClock.setClockColors(primary, mContext.getResources().getColor(R.color.lockscreen_clock_accent_color));
+        } else {
+            final int secondary = mPalette.getSecondaryColor();
+            mLockClock.setTextColor(secondary);
+            mAnalogClock.setClockColors(primary, secondary);
+        }
     }
 
     @Override

packages/SystemUI/src/com/android/keyguard/clock/DefaultBoldClockController.java
@@ -26,11 +26,14 @@ import android.text.Html;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.TextClock;
+import android.provider.Settings;
+import android.content.Context;
 
 import com.android.internal.colorextraction.ColorExtractor;
 import com.android.systemui.R;
 import com.android.systemui.colorextraction.SysuiColorExtractor;
 import com.android.systemui.plugins.ClockPlugin;
+import com.android.internal.util.derp.derpUtils;
 
 import java.util.TimeZone;
 
@@ -72,6 +75,8 @@ public class DefaultBoldClockController implements ClockPlugin {
      */
     private TextClock mClock;
 
+    private Context mContext;
+
     /**
      * Create a DefaultClockController instance.
      *
@@ -80,10 +85,11 @@ public class DefaultBoldClockController implements ClockPlugin {
      * @param colorExtractor Extracts accent color from wallpaper.
      */
     public DefaultBoldClockController(Resources res, LayoutInflater inflater,
-            SysuiColorExtractor colorExtractor) {
+            SysuiColorExtractor colorExtractor, Context context) {
         mResources = res;
         mLayoutInflater = inflater;
         mColorExtractor = colorExtractor;
+        mContext = context;
     }
 
     private void createViews() {
@@ -158,7 +164,11 @@ public class DefaultBoldClockController implements ClockPlugin {
 
     @Override
     public void setTextColor(int color) {
-        mClock.setTextColor(color);
+        if(derpUtils.useLockscreenClockAccentColor(mContext)) {
+            mClock.setTextColor(mContext.getResources().getColor(R.color.lockscreen_clock_accent_color));
+        } else {
+            mClock.setTextColor(color);
+        }
     }
 
     @Override

packages/SystemUI/src/com/android/keyguard/clock/DefaultClockController.java
@@ -25,11 +25,14 @@ import android.graphics.Typeface;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.TextView;
+import android.provider.Settings;
+import android.content.Context;
 
 import com.android.internal.colorextraction.ColorExtractor;
 import com.android.systemui.R;
 import com.android.systemui.colorextraction.SysuiColorExtractor;
 import com.android.systemui.plugins.ClockPlugin;
+import com.android.internal.util.derp.derpUtils;
 
 import java.util.TimeZone;
 
@@ -73,6 +76,8 @@ public class DefaultClockController implements ClockPlugin {
      */
     private TextView mTextDate;
 
+     private Context mContext;
+
     /**
      * Create a DefaultClockController instance.
      *
@@ -81,10 +86,11 @@ public class DefaultClockController implements ClockPlugin {
      * @param colorExtractor Extracts accent color from wallpaper.
      */
     public DefaultClockController(Resources res, LayoutInflater inflater,
-            SysuiColorExtractor colorExtractor) {
+            SysuiColorExtractor colorExtractor, Context context) {
         mResources = res;
         mLayoutInflater = inflater;
         mColorExtractor = colorExtractor;
+        mContext = context;
     }
 
     private void createViews() {
@@ -155,8 +161,12 @@ public class DefaultClockController implements ClockPlugin {
 
     @Override
     public void setTextColor(int color) {
-        mTextTime.setTextColor(color);
-        mTextDate.setTextColor(color);
+        if(derpUtils.useLockscreenClockAccentColor(mContext)) {
+            mTextTime.setTextColor((mContext.getResources().getColor(R.color.lockscreen_clock_accent_color)));
+        } else {
+            mTextTime.setTextColor(color);
+            mTextDate.setTextColor(color);
+        }
     }
 
     @Override

packages/SystemUI/src/com/android/keyguard/clock/SfunyClockController.java
@@ -26,11 +26,14 @@ import android.text.Html;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.TextClock;
+import android.provider.Settings;
+import android.content.Context;
 
 import com.android.internal.colorextraction.ColorExtractor;
 import com.android.systemui.R;
 import com.android.systemui.colorextraction.SysuiColorExtractor;
 import com.android.systemui.plugins.ClockPlugin;
+import com.android.internal.util.derp.derpUtils;
 
 import java.util.TimeZone;
 
@@ -73,6 +76,8 @@ public class SfunyClockController implements ClockPlugin {
     private TextClock mHourClock;
     private TextClock mMinuteClock;
 
+    private Context mContext;
+
     /**
      * Create a DefaultClockController instance.
      *
@@ -81,10 +86,11 @@ public class SfunyClockController implements ClockPlugin {
      * @param colorExtractor Extracts accent color from wallpaper.
      */
     public SfunyClockController(Resources res, LayoutInflater inflater,
-            SysuiColorExtractor colorExtractor) {
+            SysuiColorExtractor colorExtractor, Context context) {
         mResources = res;
         mLayoutInflater = inflater;
         mColorExtractor = colorExtractor;
+        mContext = context;
     }
 
     private void createViews() {
@@ -159,8 +165,13 @@ public class SfunyClockController implements ClockPlugin {
 
     @Override
     public void setTextColor(int color) {
-        mHourClock.setTextColor(color);
-        mMinuteClock.setTextColor(color);
+        if(Utils.useLockscreenClockAccentColor(mContext)) {
+            mLockClock.setTextColor(mContext.getResources().getColor(R.color.lockscreen_clock_accent_color));
+            mAnalogClock.setClockColors(primary, mContext.getResources().getColor(R.color.lockscreen_clock_accent_color));
+        } else {
+            mHourClock.setTextColor(color);
+            mMinuteClock.setTextColor(color);
+        }
     }
 
     @Override

packages/SystemUI/src/com/android/keyguard/clock/TypeClockController.java
@@ -25,11 +25,14 @@ import android.util.MathUtils;
 import android.graphics.Typeface;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.provider.Settings;
+import android.content.Context;
 
 import com.android.internal.colorextraction.ColorExtractor;
 import com.android.systemui.R;
 import com.android.systemui.colorextraction.SysuiColorExtractor;
 import com.android.systemui.plugins.ClockPlugin;
+import com.android.internal.util.derp.derpUtils;
 
 import java.util.TimeZone;
 
@@ -73,6 +76,8 @@ public class TypeClockController implements ClockPlugin {
     private View mView;
     private TypographicClock mTypeClock;
 
+    private Context mContext;
+
     /**
      * Small clock shown on lock screen above stack scroller.
      */
@@ -91,10 +96,11 @@ public class TypeClockController implements ClockPlugin {
      * @param colorExtractor Extracts accent color from wallpaper.
      */
     TypeClockController(Resources res, LayoutInflater inflater,
-            SysuiColorExtractor colorExtractor) {
+            SysuiColorExtractor colorExtractor, Context context) {
         mResources = res;
         mLayoutInflater = inflater;
         mColorExtractor = colorExtractor;
+        mContext = context;
         mStatusBarHeight = res.getDimensionPixelSize(R.dimen.status_bar_height);
         mKeyguardLockPadding = res.getDimensionPixelSize(R.dimen.keyguard_lock_padding);
         mKeyguardLockHeight = res.getDimensionPixelSize(R.dimen.keyguard_lock_height);
@@ -184,8 +190,13 @@ public class TypeClockController implements ClockPlugin {
 
     @Override
     public void setTextColor(int color) {
-        mTypeClock.setTextColor(color);
-        mLockClock.setTextColor(color);
+        if(derpUtils.useLockscreenClockAccentColor(mContext)) {
+            mTypeClock.setTextColor(mContext.getResources().getColor(R.color.lockscreen_clock_accent_color));
+            mLockClock.setTextColor(mContext.getResources().getColor(R.color.lockscreen_clock_accent_color));
+        } else {
+            mTypeClock.setTextColor(color);
+            mLockClock.setTextColor(color);
+        }
     }
 
     @Override
@@ -203,8 +214,12 @@ public class TypeClockController implements ClockPlugin {
             return;
         }
         final int color = colorPalette[Math.max(0, colorPalette.length - 5)];
-        mTypeClock.setClockColor(color);
-        mLockClock.setClockColor(color);
+        if(derpUtils.useLockscreenClockAccentColor(mContext)) {
+            mTypeClock.setClockColor(mContext.getResources().getColor(R.color.lockscreen_clock_accent_color));
+        } else {
+            mTypeClock.setClockColor(color);
+            mLockClock.setClockColor(color);
+        }
     }
 
     @Override